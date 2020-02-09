ef4900693a52609613dd563934c43571f5014d88
Jérémy DIOT <diot.jeremy@gmail.com>
2020-02-09 17:33:36 +0100

    add unique pair command

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 trigger and function for hash and verify password into database
 
 ## uniquePair.slq
-trigger and function for unique pair of value constraint in join table
+constraint for unique pair of values
--- uniquePair.sql
+++ uniquePair.sql
@@ -1,5 +1,10 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email text NOT NULL,
-  password text NOT NULL
+-- environment
+CREATE TABLE keys (
+  key1 INT NOT NULL,
+  key2 INT NOT NULL,
+  CONSTRAINT unique_key_pair UNIQUE(key1,key2)
 );
+
+-- test
+INSERT INTO keys (key1,key2) VALUES (1,2);
+INSERT INTO keys (key1,key2) VALUES (1,2);
